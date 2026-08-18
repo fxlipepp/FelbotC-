d84c91cdbfc6fa3e3270f7d5bf7a9edd79c2cba7
Fxlipe0 <fg0853333@gmail.com>
2026-08-18 19:24:49 +0000

    Actualiza comando info

--- commands/info.js
+++ commands/info.js
@@ -1,16 +1,17 @@
 async function infoCommand(sock, chatId, message) {
     const infoText = `                 夜
-          ── 𝐅𝐄𝐋𝐁𝐎𝐓 ──
+    ── 𝐅𝐄𝐋𝐁𝐎𝐓 ──
 
               𝟎𝟎𝟏
         𝐅𝐱𝐥𝐢𝐩𝐞 夜
-          𝑭𝒐𝒖𝒏𝒅𝒆𝒓
+          𝑪𝒓𝒆𝒂𝒅𝒐𝒓
 
               𝟎𝟎𝟐
        Yamileth 🪷
-        𝑪𝒐-𝑭𝒐𝒖𝒏𝒅𝒆𝒓
+        𝑪𝒐-𝑪𝒓𝒆𝒂𝒅𝒐𝒓𝒂
 
-          ⟡ ───── ⟡
+
+      ⟡ ───── ⟡
              夜
         𝐄𝐧𝐝 𝐨𝐟 𝐈𝐧𝐟𝐨`;
 
