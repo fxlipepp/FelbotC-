254b2c2f9f764064991766bd6dbebebb5a8fc6e1
fxlipe1 <instagramorgofc@gmail.com>
2026-09-23 07:04:37 +0000

    Actualizar menu de Felbot

--- commands/menu.js
+++ commands/menu.js
@@ -482,6 +482,7 @@ async function handleMenuButton(sock, chatId, buttonId, message) {
                 __dirname,
                 '..',
                 'assets',
+                'gifs',
                 'menucompleto',
                 'menu.mp4'
             )
@@ -547,6 +548,7 @@ Aquí encontrarás herramientas, administración, entretenimiento y mucho más.
             __dirname,
              '..',
                 'assets',
+                'gifs',
                 'menucompleto',
                 'menu.mp4'
         )
